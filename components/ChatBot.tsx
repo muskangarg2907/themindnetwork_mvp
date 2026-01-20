c82035c52d9b1815336318696e98397b5365abe2
Your Name <your-email@example.com>
2026-01-20 13:00:04 +0530

    Delete ChatBot.tsx from repo

--- components/ChatBot.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { UserProfile } from '../types';
-
-interface ChatBotProps {
-  profile: UserProfile;
-}
-
-export const ChatBot: React.FC<ChatBotProps> = () => null;
